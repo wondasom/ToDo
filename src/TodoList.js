f6ff6d263e1c5716165c23157fb0111aceb343de
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-13 20:30:28 +0200

    add onKeyPress in the input

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,84 +5,91 @@ import todoData from "./todoData";
 import ProgressBar from "./ProgressBar";
 
 const TodoList = () => {
-  const [todos, setTodos] = useState(todoData);
-  const [newTodoText, setNewTodoText] = useState("");
+	const [todos, setTodos] = useState(todoData);
+	const [newTodoText, setNewTodoText] = useState("");
 
-  const toggleTodo = text => {
-    const newTodos = _.cloneDeep(todos);
-    const todo = _.find(newTodos, { text });
+	const toggleTodo = (text) => {
+		const newTodos = _.cloneDeep(todos);
+		const todo = _.find(newTodos, { text });
 
-    // Handling error, if no todo found.
-    if (!todo) {
-      return;
-    }
+		// Handling error, if no todo found.
+		if (!todo) {
+			return;
+		}
 
-    todo.isDone = !todo.isDone;
-    setTodos(newTodos);
-  };
+		todo.isDone = !todo.isDone;
+		setTodos(newTodos);
+	};
 
-  const addTodo = () => {
-    // We return, if the text is empty, or
-    // if there's a todo with this text already.
-    if (!newTodoText || _.find(todos, { text: newTodoText })) {
-      setNewTodoText("");
-      return;
-    }
+	const addTodo = () => {
+		// We return, if the text is empty, or
+		// if there's a todo with this text already.
+		if (!newTodoText || _.find(todos, { text: newTodoText })) {
+			setNewTodoText("");
+			return;
+		}
 
-    const newTodos = [
-      ...todos,
-      {
-        text: newTodoText,
-        isDone: false
-      }
-    ];
+		const newTodos = [
+			...todos,
+			{
+				text: newTodoText,
+				isDone: false
+			}
+		];
 
-    setTodos(newTodos);
-    setNewTodoText("");
-  };
+		setTodos(newTodos);
+		setNewTodoText("");
+	};
 
-  const removeTodo = text => {
-    const newTodos = _.reject(todos, { text });
-    setTodos(newTodos);
-  };
+	const handleEnter = (event) => {
+		if (event.key === "Enter") {
+			addTodo();
+		}
+	};
 
-  const getPercentage = () => {
-    // We return 100%, instead of dividing by 0. ☝🏼
-    // There are no tasks, so we're 100% done!
-    if (todos.length === 0) {
-      return 100;
-    }
+	const removeTodo = (text) => {
+		const newTodos = _.reject(todos, { text });
+		setTodos(newTodos);
+	};
 
-    return (_.filter(todos, { isDone: true }).length / todos.length) * 100;
-  };
+	const getPercentage = () => {
+		// We return 100%, instead of dividing by 0. ☝🏼
+		// There are no tasks, so we're 100% done!
+		if (todos.length === 0) {
+			return 100;
+		}
 
-  return (
-    <>
-      <ProgressBar percentage={getPercentage()} />
-      <ul>
-        {todos.map(todo => (
-          <Todo
-            key={todo.text}
-            text={todo.text}
-            isDone={todo.isDone}
-            onToggleClick={toggleTodo}
-            onRemoveClick={removeTodo}
-          />
-        ))}
-      </ul>
-      <div className="appendToDo">
-        <input
-          className="input"
-          type="text"
-          value={newTodoText}
-          onInput={e => setNewTodoText(e.target.value)}
-        />
-        <button className="addButton" onClick={addTodo}>
-          Add
-        </button>
-      </div>
-    </>
-  );
+		return (_.filter(todos, { isDone: true }).length / todos.length) * 100;
+	};
+
+	return (
+		<>
+			<ProgressBar percentage={getPercentage()} />
+			<ul>
+				{todos.map((todo) => (
+					<Todo
+						key={todo.text}
+						text={todo.text}
+						isDone={todo.isDone}
+						onToggleClick={toggleTodo}
+						onRemoveClick={removeTodo}
+					/>
+				))}
+			</ul>
+			<div className='appendToDo'>
+				<input
+					className='input'
+					type='text'
+					value={newTodoText}
+					onInput={(e) => setNewTodoText(e.target.value)}
+					onKeyPress={handleEnter}
+				/>
+				<button className='addButton' onClick={addTodo}>
+					Add
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default TodoList;
